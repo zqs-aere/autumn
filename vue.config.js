4e930bdcbc42b554c280b1b6e693ed8222ebb14b
zqs <18611433407@163.com>
2019-06-25 10:31:11 +0800

    打包设置修改

--- vue.config.js
+++ vue.config.js
@@ -33,8 +33,8 @@ module.exports = {
       minimizer: [
         new UglifyJsPlugin({
           uglifyOptions: {
+            warnings: false,
             compress: {
-              warnings: false,
               drop_console: true, // console
               drop_debugger: false
             }
